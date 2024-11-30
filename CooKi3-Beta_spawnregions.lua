45ac1f286c21c4848724d3363e79613d909b17fd
t3qquq <t3qquq@gmail.com>
2024-12-01 08:45:38 +0900

    Fix: 맵 개편

--- CooKi3-Beta_spawnregions.lua
+++ CooKi3-Beta_spawnregions.lua
@@ -1,5 +1,6 @@
 function SpawnRegions()
 	return {
-		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+--		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Point 1", file = "media/maps/cooki3/spawnpoints.lua" },
 	}
 end
